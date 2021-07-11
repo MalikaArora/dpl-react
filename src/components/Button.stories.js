8bf73647bf5ff6f1ad68479f21c0cfcff5015880
Saratkannan <48374050+Saratkannan@users.noreply.github.com>
2021-07-12 03:54:52 +0530

    Update Button.stories.js

--- src/components/Button.stories.js
+++ src/components/Button.stories.js
@@ -1,20 +1,22 @@
-import React from 'react';
 
-import Button from '@dpl/react-button';
+import React from "react";
+import Button from "@dpl/react-button";
 
 export default {
-  title: 'Button',
-  component: Button,
-  
+  title: "Button",
+  args: {
+      disabled: false,
+      primary: true,
+      children: 'Click Here',
+      type: "button",
+    
+  },
 };
 
-const Template = (args) => <Button {...args} />;
-
-export const PrimaryBtn = Template.bind({});
-PrimaryBtn.args = {
-  disabled: false,
-  primary: true,
-  children: 'Click Here',
-  type: "button",
-  onClick: alert('You clicked'),
+export const Basic = (props) => {
+  return (
+    <>
+      <Button onClick={() => {alert('You clicked')}} {...props}></Button>
+    </>
+  );
 };
