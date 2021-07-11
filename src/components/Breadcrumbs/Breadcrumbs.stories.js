c061a4f3d889bcb7837e878d88ddee882248d39d
Saratkannan <48374050+Saratkannan@users.noreply.github.com>
2021-07-12 03:51:08 +0530

    Create Breadcrumbs.stories.js

---
+++ src/components/Breadcrumbs/Breadcrumbs.stories.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ShowBreadcrumbs from "./Breadcrumbs.js";
+
+export default {
+    title: 'Breadcrumbs',
+}
+
+export const Basic = () => <ShowBreadcrumbs></ShowBreadcrumbs>
