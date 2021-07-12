c577cee6f4bd2f6b2323aa0a01aa5a0e6083db4f
Saratkannan <48374050+Saratkannan@users.noreply.github.com>
2021-07-12 15:14:40 +0530

    Update Content.stories.js

--- src/components/Content.stories.js
+++ src/components/Content.stories.js
@@ -3,12 +3,15 @@ import Content from "@dpl/react-content";
 
 export default {
   title: "Content",
+  args: {
+    maxWidth: '80px',  
+  },
 };
 
 export const Basic = (props) => {
   
   return (
-    <Content>
+    <Content {...props}> 
        <h1>Heading 1</h1> 
        <h2>Heading 2</h2>
        <h3>Heading 3</h3>
