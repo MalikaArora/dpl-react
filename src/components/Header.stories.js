1553bd0717c88d56b595494a0127b7a29a4819ed
Saratkannan <48374050+Saratkannan@users.noreply.github.com>
2021-07-12 12:30:58 +0530

    Update Header.stories.js

--- src/components/Header.stories.js
+++ src/components/Header.stories.js
@@ -1,18 +1,19 @@
-import React from 'react';
-
-import Header from '@dpl/react-dpl-header';
+import React from "react";
+import Header from "@dpl/react-dpl-header";
 
 export default {
-  title: 'Header',
-  component: Header,
+  title: "Header",
+  args: {
+    appName: "HEADER TITLE",
+    logoUrl: "/",
+  },
 };
 
-const Template = (args) => <Header {...args} />;
-
-export const Basic = Template.bind({});
-Basic.args = {
-  appName: "TEST_HEADER",
-  logoUrl: "/",
-  onLogoCLick: alert('You clicked'), 
-
+export const Basic = (props) => {
+  
+  return (
+    <>
+      <Header onLogoClick={() => {alert('You clicked the logo')}} {...props}></Header>
+    </>
+  );
 };
