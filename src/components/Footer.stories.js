19cbdc19cd97d5b00dfae05d989fc8a385fa7866
Saratkannan <48374050+Saratkannan@users.noreply.github.com>
2021-07-12 12:40:59 +0530

    Update Footer.stories.js

--- src/components/Footer.stories.js
+++ src/components/Footer.stories.js
@@ -1,5 +1,4 @@
 import React from 'react';
-
 import Footer from '@dpl/react-dpl-footer';
 
 export default {
@@ -11,6 +10,6 @@ const Template = (args) => <Footer {...args} />;
 
 export const Basic = Template.bind({});
 Basic.args = {
-  //appName: "TEST_FOOTER",
-  
+  appName: "FOOTER TITLE",
+  children: <div>Your use of this product is governed by the terms of your company agreement. You may not use or disclose this product or allow others to use it or disclose it, except as permitted by your agreement with Optum.</div>,
 };
