89bc0f61a42651a03dcfed47970b6413484395af
Saratkannan <48374050+Saratkannan@users.noreply.github.com>
2021-07-12 11:17:36 +0530

    Create WindowEvent.stories.js

---
+++ src/components/WindowEvent.stories.js
@@ -0,0 +1,45 @@
+import React from "react";
+import WindowEvent from "@dpl/react-window-event";
+
+export default {
+  title: "WindowEvent",
+  args: {
+    type: "resize",
+  },
+  argTypes: {
+    type: {
+      control: { type: "radio", options: ["resize", "scroll"] },
+      type: { required: true },
+    },
+  },
+};
+
+export const Basic = (props) => {
+  const [resizeCount, setResizeCount] = React.useState(0);
+  const [scrollCount, setScrollCount] = React.useState(0);
+
+  function handler() {
+    if (props.type === "resize") {
+      setResizeCount((count) => count + 1);
+    } else {
+      setScrollCount((count) => count + 1);
+    }
+  }
+
+  return (
+    <WindowEvent {...props} handler={handler}>
+      <div style={{ height: "2000px" }}>
+        <table>
+          <tr>
+            <th>resize</th>
+            <th>scroll</th>
+          </tr>
+          <tr>
+            <td>{resizeCount}</td>
+            <td>{scrollCount}</td>
+          </tr>
+        </table>
+      </div>
+    </WindowEvent>
+  );
+};
