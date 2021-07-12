3bd82fef637aba1a3ecb32ca4ab53a5c7a6f7cb8
Saratkannan <48374050+Saratkannan@users.noreply.github.com>
2021-07-12 13:31:22 +0530

    Update Content.stories.js

--- src/components/Content.stories.js
+++ src/components/Content.stories.js
@@ -1,19 +1,29 @@
-import React from 'react';
-
-import Content from '@dpl/react-content';
+import React from "react";
+import Content from "@dpl/react-content";
 
 export default {
-  title: 'Content',
-  component: Content,
-  
+  title: "Content",
 };
 
-const Template = (args) => <Content {...args} />;
-
-export const Basic = Template.bind({});
-Basic.args = {
-  maxWidth: '100px',
-  children: <div>
-    Hello
-  </div>,
+export const Basic = (props) => {
+  
+  return (
+    <Content>
+       <h1>Heading 1</h1> 
+       <h2>Heading 2</h2>
+       <h3>Heading 3</h3>
+       <h4>Heading 4</h4>
+       <h5>Heading 5</h5>
+       <h6>Heading 6</h6>
+       <p>Paragragh</p>
+       <blockquote>Blockquote</blockquote>
+       <a href="/">A tag</a>
+       <ul>Unordered List
+           <li>List Items</li>
+       </ul>
+       <ol>Ordered List
+           <li>List Items</li>
+       </ol>
+    </Content> 
+  );
 };
